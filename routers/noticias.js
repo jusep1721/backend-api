75ad251fb5ebfb80ec35da4b3ba6e563585ff0c0
Jose Dominguez <Dominguezjose2708@outlook.com>
2021-11-16 01:07:20 -0300

    UPDATE backend

--- routers/noticias.js
+++ routers/noticias.js
@@ -6,16 +6,16 @@ noticiaAPP.get('/noticias', (req, res) => {
     noticia.find().then(e => res.json(e)).catch(() => res.status(404).send("No se encontro ningun dato"))
 })
 
-// noticiaAPP.post('/postNoticia', (req, res) => {
-//     const body = req.body
-//     const newNoticia = creandoNoticia(body)
-//     newNoticia.then(res.status(401).send("Nueva noticia registrada")).catch(() => res.status(204).send("Error al cargar la noticia"))
-// })
+noticiaAPP.post('/postNoticia', (req, res) => {
+    const body = req.body
+    const newNoticia = creandoNoticia(body)
+    newNoticia.then(res.status(401).send("Nueva noticia registrada")).catch(() => res.status(204).send("Error al cargar la noticia"))
+})
 
-// const creandoNoticia = async body => {
-//     const newNoticia = new noticia(body)
-//     return await newNoticia.save()
-// }
+const creandoNoticia = async body => {
+    const newNoticia = new noticia(body)
+    return await newNoticia.save()
+}
 
 module.exports = { noticiaAPP }
 
